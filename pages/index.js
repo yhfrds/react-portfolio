970add08c5437536a97b1de57deb4941c3e7ed67
yhfrds <yudhafirdaus736@gamil.com>
2024-02-10 22:19:48 +0700

    important part of the website is added

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import Head from "next/head";
 import { BsFillMoonStarsFill } from "react-icons/bs";
+import { AiFillLinkedin, AiFillMail, AiFillInstagram } from "react-icons/ai";
+import Image from "next/image";
+import deved from "../public/dev-ed-wave.png";
 
 export default function Home() {
   return (
@@ -13,14 +16,14 @@ export default function Home() {
       <main className="bg-white px-10">
         <section className=" min-h-screen">
           <nav className="py-10 mb-12 flex justify-between">
-            <h1 className="text-xl">developedByYudha</h1>
+            <h1 className="text-xl font-burtons">developedbyYudha</h1>
             <ul className="flex items-center">
               <li>
                 <BsFillMoonStarsFill className="cursor-pointer text-2xl" />
               </li>
               <li>
                 <a
-                  className=" bg-gradient-to-r from-cyan-500 to-red-400 text-white px-4 py-2 rounded-md ml-8"
+                  className=" bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
                   href="#"
                 >
                   Resume
@@ -28,6 +31,41 @@ export default function Home() {
               </li>
             </ul>
           </nav>
+
+          <div className="text-center p-10 ">
+            <h2 className="text-5xl py-2 text-teal-600 font-medium">
+              Yudha Firdaus
+            </h2>
+            <h3 className="text-2xl py-2">Frontend Developer.</h3>
+            <p className="text-md py-5 leading-8 text-gray-800">
+              Hey there! I'm a computer science student who loves coding and
+              crafting beautiful user experiences. Let's bring your projects to
+              life together!
+            </p>
+          </div>
+
+          <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 cursor-pointer">
+            <AiFillInstagram />
+            <AiFillLinkedin />
+            <AiFillMail />
+          </div>
+
+          {/* mx auto will center it */}
+          <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden">
+            <Image src={deved} layout="fill" objectFit="cover" />
+          </div>
+        </section>
+
+        <section>
+          <div>
+            <h3 className="text-3xl py-1">Services I offer</h3>
+            <p className="text-md py-2 leading 8 text-gray-800">
+              Since the beginning of my journey as{" "}
+              <span className="text-teal-500">computer science</span> student
+              and doing an internship in a company in Aachen, i've done some
+              work in the frontend and backend{" "}
+            </p>
+          </div>
         </section>
       </main>
     </div>
